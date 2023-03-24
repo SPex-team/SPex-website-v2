5184b5980c27347fd98bd76c25cbfdd8caa4917c
LiQingShan <lqsbznh@gmail.com>
2023-03-24 13:45:29 +0800

    feat: modify SEO deacription

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,17 @@ export default function App({ Component, pageProps }) {
         <meta property="og:title" content="SPex" />
         <meta property="twitter:title" content="SPex" />
         <meta property="og:site_name" content="SPex" />
-        <meta name="twitter:site" content="SPex" />
+        <meta name="twitter:site" content="https://www.spex.website/" />
         <meta property="og:image" content="/favicon.svg" />
         <meta name="twitter:image:src" content="/favicon.svg" />
-        <meta property="og:description" content="description" />
-        <meta property="twitter:description" content="description" />
+        <meta
+          property="og:description"
+          content="SPex is an exchange for storage providers on FVM to allow SPs to trade their account belongings such as ID, beneficiary, etc. for financing, shorting; incorporating with other FVM products to offer a one-stop-shop financial services "
+        />
+        <meta
+          property="twitter:description"
+          content="SPex is an exchange for storage providers on FVM to allow SPs to trade their account belongings such as ID, beneficiary, etc. for financing, shorting; incorporating with other FVM products to offer a one-stop-shop financial services "
+        />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <Component {...pageProps} />
