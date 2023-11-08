bf7991798927b698c52aec9962d6f3dc7ec318ad
tianxiong <tianxiong@kunyaokeji.com>
2023-11-08 18:09:03 +0800

    feat: updated build

--- out/_next/static/-ZqHdBQf3y3LvB7ZaBALg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-43cb614b8ce88c57.js","static/chunks/pages/index-d1c7d05254c29bdc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
